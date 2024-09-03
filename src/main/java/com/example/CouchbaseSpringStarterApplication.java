37a8bed4e91ea29a023471b9536612d3088b0a1e
Laurent Doguin <laurent.doguin@gmail.com>
2024-09-03 10:53:16 +0200

    update ga with proper env variables

--- src/main/java/com/example/CouchbaseSpringStarterApplication.java
+++ src/main/java/com/example/CouchbaseSpringStarterApplication.java
@@ -1,13 +1,10 @@
 package com.example;
 
-import com.couchbase.client.java.Scope;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
